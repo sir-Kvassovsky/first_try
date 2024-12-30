4a278c891dd8a1e91351aa02fcb96d9d56f06ca2
sir-Kvassovsky <159709815+sir-Kvassovsky@users.noreply.github.com>
2024-12-30 16:29:06 +0300

    be

--- MySprite2D.cs
+++ MySprite2D.cs
@@ -5,9 +5,18 @@ public partial class MySprite2D : Sprite2D
 {
 	private int _speed = 400;
 	private float _angularSpeed = Mathf.Pi;
+	private void OnButtonPressed()	
+	{
+		SetProcess(!IsProcessing());
+	}
 	public override void _Process(double delta)
 	{
 		var direction = 0;
+		var velocity = Vector2.Zero;
+		if (Input.IsActionPressed("ui_up"))
+		{
+			velocity = Vector2.Up.Rotated(Rotation) * _speed;
+		}
 		if (Input.IsActionPressed("ui_left"))
 		{
 			direction = -1;
@@ -18,8 +27,7 @@ public partial class MySprite2D : Sprite2D
 		}
 
 		Rotation += _angularSpeed * direction * (float)delta;
-		// var velocity = Vector2.Up.Rotated(Rotation) * _speed;
-		// Position += velocity * (float)delta;
+		Position += velocity * (float)delta;
 	}
 
 }
